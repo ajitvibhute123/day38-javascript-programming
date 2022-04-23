292aaa8680e984ce5bca0e15c6925df9ba017ef5
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-04-23 23:55:14 +0530

    Added factorial file program

---
+++ ForLoop/Factorial.js
@@ -0,0 +1,15 @@
+//Reading input from console using prompt-sync NodeJS Module
+let input = parseInt(prompt("Enter number to find factorial -> "))
+
+if (input == 0){
+    console.log("Factorial of zero = 1");    
+}else if (input == 1){
+    console.log(`the factorial is = ${0}`);
+}
+else{
+    let fact = 1 ;
+    for (let i = 2 ; i <= input ; i++){
+        fact = fact*i;
+    }
+    console.log(`the factorial of ${input} is ${fact}.`);
+}
