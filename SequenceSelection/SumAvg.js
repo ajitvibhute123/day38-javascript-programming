5bb13732142227735046024ab772905f6b430e60
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-04-24 00:44:55 +0530

    Added sum avg file by using sequence selection.

---
+++ SequenceSelection/SumAvg.js
@@ -0,0 +1,11 @@
+function generateRandomNumber() {
+    return Math.round(Math.random() * 89 + 10);
+}
+let sum = 0;
+for (index = 1; index <= 5; index++) {
+    let number = generateRandomNumber();
+    console.log("Number" + index + " = " + number);
+    sum += number;
+}
+console.log("The sum of 5 random 2 digit value is = " + sum);
+console.log("The average of 5 random 2 digit value is = " + sum / 5);
